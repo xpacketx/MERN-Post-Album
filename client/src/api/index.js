d8b8f3aed1b2a0471c1725165a4034ba06e23d43
xpacketx <44978672+xpacketx@users.noreply.github.com>
2021-03-23 23:13:58 +1100

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'https://mern-post-album.herokuapp.com/api/posts'
+const url = 'url provided by Heroku API on deployment'
 
 export const fetchPosts = () => axios.get(url)
 export const createPost = (newPost) => axios.post(url, newPost)
